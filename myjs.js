4b7e111037ade698aa3346db7487b8492c2c74d7
sanjibiglove <sanjibiglove@gmail.com>
2018-07-29 20:53:21 -0400

    change table size

---
+++ myjs.js
@@ -0,0 +1,10 @@
+alert("Hello,Mizuki!");
+//alert(1+1);
+//alert(Math.sqrt(1+1));
+//alert(Math.sqrt(2+3));
+//alert(Math.sqrt(1/0));
+//alert(Math.sqrt(0/0));
+//alert(Math.sqrt('2'+'3'));
+//alert(Math.sqrt(Number('2')+Number('3')));
+//alert(Math.sqrt('2'+3));
+//alert(Math.sqrt(Number('2')+3));      
